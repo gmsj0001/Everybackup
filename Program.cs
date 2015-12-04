178af8ed73df38e924e058c52eb345d7e6cce16a
gmsj0001 <gmsj0001@163.com>
2015-12-04 16:51:34 +0800

    v1.0 commit

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Everybackup
+{
+    class Program
+    {
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string lpszSection, string lpszKey, string lpszDefault, StringBuilder lpReturnedString, int cchReturnBuffer, string lpszFile);
+        [DllImport("Everything32.dll", CharSet = CharSet.Unicode)]
+        public static extern int Everything_SetSearchW(string lpSearchString);
+        [DllImport("Everything32.dll")]
+        public static extern bool Everything_QueryW(bool bWait);
+        [DllImport("Everything32.dll")]
+        public static extern int Everything_GetNumResults();
+        [DllImport("Everything32.dll", CharSet = CharSet.Unicode)]
+        public static extern void Everything_GetResultFullPathNameW(int nIndex, StringBuilder lpString, int nMaxCount);
+
+        static string GetIniString(string key)
+        {
+            var retVal = new StringBuilder(260);
+            GetPrivateProfileString("Everybackup", key, "", retVal, retVal.Capacity, Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + "\\Everybackup.ini");
+            return retVal.ToString();
+        }
+
+        static HashSet<string> EverythingSearch(string search)
+        {
+            Everything_SetSearchW(search);
+            Everything_QueryW(true);
+            var set = new HashSet<string>();
+            var sum = Everything_GetNumResults();
+            for (int i = 0; i < sum; ++i)
+            {
+                var str = new StringBuilder(260);
+                Everything_GetResultFullPathNameW(i, str, 260);
+                set.Add(str.ToString());
+            }
+            return set;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+@"=================================
+Everybackup v1.0 beta by gmsj0001
+         [http://lxf.me]
+=================================");
+            var files = new HashSet<string>();
+            var confs = EverythingSearch("wfn:" + GetIniString("ListFile")).ToList();
+            confs.Sort();
+            foreach (var conf in confs)
+            {
+                var lines = File.ReadAllLines(conf).Select(line => line.Trim()).Where(line => line != "");
+                var path = Path.GetDirectoryName(conf).TrimEnd('\\') + '\\';
+                foreach (var line in lines)
+                {
+                    if (line[0] != '|')
+                    {
+                        Console.WriteLine("Include: " + path + line);
+                        files.UnionWith(EverythingSearch("file:" + path + line));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Exclude: " + path + line.Substring(1));
+                        files.ExceptWith(EverythingSearch("file:" + path + line.Substring(1)));
+                    }
+
+                }
+            }
+            var list = files.ToList();
+            list.Sort();
+            File.WriteAllLines("Everybackup.lst", list.ToArray(), Encoding.Default);
+
+            var execProgram = GetIniString("ExecProgram");
+            if (execProgram != "")
+                System.Diagnostics.Process.Start(GetIniString("ExecProgram"), GetIniString("ExecArguments"));
+        }
+    }
+}
--- readme.txt
+++ readme.txt
@@ -1,1 +1,21 @@
+===========
+Everybackup
+===========
+
 A backup file list configuration tool using Everything for accelerating.
+
+
+Usage
+=====
+
+0. You need Everything installed and launched. .NET 3.5 needed (for Linq support).
+1. Download release (or compile by yourself and put Everything32.dll in the directory).
+2. Create backup.lst (or other name you set in the ini file) in any folder you want to backup.
+3. Edit backup.lst with include lines (eg. photos *.doc) or exclude lines (start with a pipe, eg. |nouse |*.obj).
+4. Run the program and a file list named Everyback.lst will be generated.
+5. You can set ExecProgram and ExecArguments in the ini file to run archive software automatically after file list generated.
+
+More?
+=====
+
+No more. This project is quite simple (less than 100 lines) so you can fork and add anything you want.
